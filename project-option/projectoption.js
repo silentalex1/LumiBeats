f5df4b97e02edd9bb0dd23361be714771e3f403a
Zal <101911964+silentalex1@users.noreply.github.com>
2025-12-15 16:34:37 -0500

    Create projectoption.js

---
+++ project-option/projectoption.js
@@ -0,0 +1,7 @@
+document.getElementById('yes-btn').addEventListener('click', () => {
+    window.location.href = 'saved-projects.html';
+});
+
+document.getElementById('no-btn').addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
