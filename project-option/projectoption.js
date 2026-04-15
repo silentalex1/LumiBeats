2b5cf3b0676034403ee704278d283bb838eff7bb
Zal <101911964+silentalex1@users.noreply.github.com>
2026-04-15 15:04:59 -0400

    Delete project-option directory

--- project-option/projectoption.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('yes-btn').addEventListener('click', () => {
-    window.location.href = 'saved-projects.html';
-});
-
-document.getElementById('no-btn').addEventListener('click', () => {
-    window.location.href = 'index.html';
-});
