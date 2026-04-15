36253c61fb81e4fb4c714ab499fe2b09317bbbd7
Zal <101911964+silentalex1@users.noreply.github.com>
2026-04-15 15:05:07 -0400

    Delete saved-projects directory

--- saved-projects/savedprojects.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('new-btn').addEventListener('click', () => {
-    window.location.href = 'index.html';
-});
-
-document.querySelectorAll('.project-card').forEach(card => {
-    card.addEventListener('click', () => {
-        window.location.href = 'index.html';
-    });
-});
